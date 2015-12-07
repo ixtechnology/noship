0a32cbdd3c4004198f1b2d1809076c62ee80a338
gavwhela@gmail.com <gavwhela@gmail.com>
2015-12-07 05:25:36 +0000

    First website version, about to merge new website version.

--- Application.hs
+++ Application.hs
@@ -36,6 +36,7 @@ import Handler.Fay
 import Handler.Home
 import Handler.About
 import Handler.Projects
+import Handler.Log
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
